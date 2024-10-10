d52d4111d23ee8e5d31b5eda6ea79884821e26af
zhangxvdong <zhangxvdong@bytedance.com>
2024-10-10 21:25:45 +0800

    feat: temp save

---
+++ src/routes/components/Aside/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Search from './components/Search.svelte';
+</script>
+
+<aside>
+  <Search />
+</aside>
+
+<style>
+	aside {
+		display: flex;
+		flex-direction: column;
+    justify-content: flex-start;
+		background-color: #ffffff;
+		width: 34%;
+    max-width: 350px;
+		padding: 16px;
+	}
+</style>
---
+++ src/routes/components/Footer.svelte
@@ -0,0 +1,10 @@
+<footer></footer>
+
+<style>
+	footer {
+    width: 100%;
+    height: 40px;
+		background-color: rgb(208, 218, 61);
+    flex-shrink: 0;
+	}
+</style>
